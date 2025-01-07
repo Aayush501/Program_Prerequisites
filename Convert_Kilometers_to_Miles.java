cd946624e148f96d34a909d6e25aaf555aaabafc
Aayush501 <shrivastavashu501@gmail.com>
2025-01-07 21:33:49 +0530

    solved the problem to convert kilometers value into miles

---
+++ Convert_Kilometers_to_Miles.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Convert_Kilometers_to_Miles{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int kilometers = sc.nextInt();
+		System.out.println(kilometers * 0.621371);
+	}
+}
